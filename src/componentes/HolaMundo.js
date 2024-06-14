96a9578338e4b4ebb3b797f8b2aa0e0818591f4e
Omar  Sánchez <ing_omarglez@hotmail.com>
2024-06-13 23:27:52 -0600

    Creando carpeta para nuevos componentes
    Se creo la carpeta para agregar nuevos componentes, se inicia la aplicación .

---
+++ src/componentes/HolaMundo.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function HolaMundo() {
+    
+ return (
+    <div>
+        <h1>Hola Mundo</h1>
+        <h2>Omar Sánchez</h2>
+    </div>
+ )
+}
+
+export default App;
